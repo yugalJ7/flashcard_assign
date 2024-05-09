1b6439934858d7fb9bc5c4f0af7d2b91cc0bc4d9
gingHUB <jyugal180.jy@gmail.com>
2024-05-09 19:28:08 +0530

    added breadcrum completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      vietnam: ["Be Vietnam Pro", "sans-serif"],
+    },
   },
   plugins: [],
 };
